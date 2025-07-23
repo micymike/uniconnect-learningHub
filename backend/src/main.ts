23e41670b65651b092bac887d78a0af8397b3ae3
geoffowuor <geoffreygoovy@gmail.com>
2025-07-23 11:58:10 +0300

    removed CORS config to test the APIs localy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS for frontend
-  app.enableCors({ origin: 'http://localhost:3000' });
+  //app.enableCors({ origin: 'http://localhost:3000' });
 
   // Swagger configuration
   const config = new DocumentBuilder()
