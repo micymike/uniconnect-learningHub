f60abdf29200c2c3677c9408ff1ae238b481a86d
geoffowuor <geoffreygoovy@gmail.com>
2025-08-02 09:31:27 +0300

    created data transfer objects for user registration based on supabase auth

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,13 +1,7 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
-import { UserRole } from '../interfaces/user.interface';
-
-export class CreateUserDto {
-  @IsEmail()
+export class RegisterDto {
   email: string;
-
-  @IsString()
-  @MinLength(8)
   password: string;
-
-  role?: UserRole;
+  role: string;
+  fullName?: string;
 }
+
